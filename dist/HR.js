67da91cc7dd0047f0e5d917b8c90499867f33886
Hamza Ashfaq <hamza.ashfaq@khaleef.com>
2021-09-29 18:44:19 +0500

    initial commit

---
+++ dist/HR.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HR = () => /*#__PURE__*/React.createElement("div", null, "hello world");
+
+export default HR;
---
+++ src/component/HR.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HR = () => <div>hello world</div>;
+
+export default HR
